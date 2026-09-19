d53cce311903257689be4a9a305a5b4dc8972965
PourLePlaisir-HA <seb.github.project@gmail.com>
2026-09-19 18:18:49 +0200

    Bump frontend to v0.7.0-beta.2

--- custom_components/streaming_top_fr/www/streaming-top-fr-card.js
+++ custom_components/streaming_top_fr/www/streaming-top-fr-card.js
@@ -1,4 +1,4 @@
-const STFR_VERSION = "0.7.0-beta.1";
+const STFR_VERSION = "0.7.0-beta.2";
 class StreamingTopFrCard extends HTMLElement {
   connectedCallback(){
     if(this._statusSyncHandler)return;
