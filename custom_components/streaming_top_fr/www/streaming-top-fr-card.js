1a03f55865a706d00e615ff09830682adef15e7c
PourLePlaisir-HA <seb.github.project@gmail.com>
2026-09-20 19:20:35 +0200

    Prepare 0.8.0-beta.7

--- custom_components/streaming_top_fr/www/streaming-top-fr-card.js
+++ custom_components/streaming_top_fr/www/streaming-top-fr-card.js
@@ -1,4 +1,4 @@
-const STFR_VERSION = "0.8.0-beta.5";
+const STFR_VERSION = "0.8.0-beta.7";
 class StreamingTopFrCard extends HTMLElement {
   connectedCallback(){
     if(this._statusSyncHandler)return;
