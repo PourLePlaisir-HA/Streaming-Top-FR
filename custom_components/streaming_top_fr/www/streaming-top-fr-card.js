376b4227129b7cd1163eec31cd7f88c7724e3a7c
PourLePlaisir-HA <seb.github.project@gmail.com>
2026-09-21 20:18:44 +0200

    Set frontend version to 1.0.2-beta.1

--- custom_components/streaming_top_fr/www/streaming-top-fr-card.js
+++ custom_components/streaming_top_fr/www/streaming-top-fr-card.js
@@ -1,4 +1,4 @@
-const STFR_VERSION = "1.0.1-beta.8";
+const STFR_VERSION = "1.0.2-beta.1";
 class StreamingTopFrCard extends HTMLElement {
   connectedCallback(){
     if(this._statusSyncHandler)return;
